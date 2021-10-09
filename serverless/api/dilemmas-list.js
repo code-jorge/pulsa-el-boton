f51f8802494244e1239e8701b8cb1054dc669fc5
Jorge Aguirre <jorge.aguirre.leon@gmail.com>
2021-10-09 22:03:49 +0200

    Fix backend call

--- serverless/api/dilemmas-list.js
+++ serverless/api/dilemmas-list.js
@@ -6,7 +6,11 @@ exports.handler = async (event, context)=> {
   const db = await connect()
   const { page } = event.queryStringParameters
   const now = new Date()
-  const data = await db.collection("dilemmas").find({ date: { $lte: now } }).limit(PAGE_SIZE).skip(PAGE_SIZE*(page-1))
+  const data = await db.collection("dilemmas")
+    .find({ date: { $lte: now } })
+    .limit(PAGE_SIZE)
+    .skip(PAGE_SIZE*(page-1))
+    .toArray()
   return {
     statusCode: 200,
     body: JSON.stringify({ dilemmas: data })
